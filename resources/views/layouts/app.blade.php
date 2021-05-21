9d5fe9c1060a7f6adb4c27f883ab85099c02963c
mildred <mildredfigueroaq@gmail.com>
2021-05-21 11:02:16 -0500

    home ciclos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Live4teach') }}</title>
+    <title>{{ config('app.name', 'Trikele') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
