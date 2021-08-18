7509fa01bb560ff2b85e6ee03805c44975599ebf
Brayanstyd <brayantriana22@gmail.com>
2021-08-18 12:36:29 -0500

    verificacion de existencia de archivos

--- resources/js/components/studentCourseComponent.vue
+++ resources/js/components/studentCourseComponent.vue
@@ -432,7 +432,7 @@ export default {
         openDocument(resource) {                
             let data = resource.content.split('trikele.com');                
             let documentDownloaded = `https://trikele.edu.co${data[1]}`;
-            console.log('Documento Descargado',documentDownloaded);
+            console.log('Documento Descargado',asset(documentDownloaded));
             try {
                 downloadjs(documentDownloaded);
             } catch {}
