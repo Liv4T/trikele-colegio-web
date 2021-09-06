3089c247f1e489439d5559bccd455e0a55557cce
Brayanstyd <brayantriana22@gmail.com>
2021-09-06 10:04:51 -0500

    Modelo de messaging modificado para obtener fechas

--- app/Messaging.php
+++ app/Messaging.php
@@ -9,6 +9,12 @@ class Messaging extends Model
     //
     protected $table = 'messaging';
 
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
     protected $fillable = [
         'id_emisor', 'subject', 'message',
     ];
