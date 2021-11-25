5e321aaa1c17b3bbb51796f934a1b5fd202a62fd
Brayanstyd <brayantriana22@gmail.com>
2021-11-25 14:32:05 -0500

    compilacion de sqliteLoadLocal a develop

--- public/66.js
+++ public/66.js
@@ -154,7 +154,7 @@ __webpack_require__.r(__webpack_exports__);
 
     this.classs = [];
     this.getClassContent();
-    axios.get('/attemps').then(function (response) {
+    axios.get("/attemps/".concat(this.id_student)).then(function (response) {
       var data = response.data;
       data.forEach(function (element) {
         _this.attemps.push({
